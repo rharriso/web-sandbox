2f37884bceb5472d3370a5f50adfa71a9f500572
Ross Harrison <rtharrison86@gmail.com>
2012-06-26 14:34:41 -0500

    Adding code to handle different file types and failures

--- imageWithWorker/imageWorkerEncodeLocally.js
+++ imageWithWorker/imageWorkerEncodeLocally.js
@@ -1,12 +1,50 @@
 importScripts("base64.js");
 
+/*
+	Property: dataPrefix
+		lookup of data prefixes for different content types
+*/
+var dataPrefix = {
+	"jpg": "data:image/jpeg;base64,",
+	"jpeg": "data:image/jpeg;base64,",
+	"png": "data:image/png;base64,",
+	"gif": "data:image/gif;base64,",
+}
+
+/*
+	Function: onMessage
+		prepare request for image data
+ */
 self.onmessage = function(e){
 	var xhr = new XMLHttpRequest();
 	xhr.overrideMimeType("text/plain; charset=x-user-defined");
     xhr.open('GET', e.data, false);
     
-    xhr.onreadystatechange = function() {
-    	postMessage("data:image/jpeg;base64,"+base64.encode(xhr.responseText));
-    };
+    //determine the suffix
+    var componentArr = e.data.split("."),
+    	suffix = componentArr[componentArr.length-1];
+    	suffix = suffix.toLowerCase();
+
+    //on complete
+    xhr.addEventListener("load", function() {
+
+    	resp = {
+    		status: xhr.status,
+    		requestURL: e.data,
+    		content: null
+    	}
+
+    	//if success, convert to base64 encoding
+		if(xhr.status === 200){
+			resp.content = dataPrefix[suffix]+base64.encode(xhr.responseText);	
+		}else{
+			resp.content = xhr.responseText
+		}		
+
+		//send json back in response
+		postMessage(JSON.stringify(resp));
+
+    }, false);
+
     xhr.send();
 }
